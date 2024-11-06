d8cd9ee365707cd7d3a2ec2215da86f2af8c0f0e
Suresh Nayak <suresh.nayak@acheron-tech.com>
2024-11-06 14:53:34 +0530

    use environment details

--- src/main/java/com/spring/jpa/mssql/project/controller/EmployeeController.java
+++ src/main/java/com/spring/jpa/mssql/project/controller/EmployeeController.java
@@ -2,10 +2,10 @@ package com.spring.jpa.mssql.project.controller;
 
 import java.util.List;
 
-import javax.ws.rs.GET;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +23,8 @@ public class EmployeeController {
 
 	@GetMapping("/all")
 	List<EmployeeModel> getAllEmployees() {
+		List<EmployeeModel> employee = repposritoy.findAll();
+		// employee.stream().filter()
 		return repposritoy.findAll();
 
 	}
@@ -31,7 +33,17 @@ public class EmployeeController {
 	List<EmployeeModel> saveEmployee(@RequestBody EmployeeModel employeeModel) {
 		repposritoy.save(employeeModel);
 		return repposritoy.findAll();
+	}
 
+	@DeleteMapping("/{id}")
+	List<EmployeeModel> deleteEmployee(@PathVariable int id) {
+		repposritoy.deleteById(id);
+		return repposritoy.findAll();
+	}
+
+	@GetMapping("/{id}")
+	EmployeeModel findEmployee(@PathVariable int id) {
+		return repposritoy.findById(id);
 	}
 
 }
